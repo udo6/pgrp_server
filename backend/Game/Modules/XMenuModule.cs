e33ac9c35b58f03cf927f0573f9e1dab90a585fa
mas on <dripudo123@gmail.com>
2023-12-14 13:56:44 +0100

    super secret anticheat update

--- backend/Game/Modules/XMenuModule.cs
+++ backend/Game/Modules/XMenuModule.cs
@@ -54,7 +54,7 @@ namespace Game.Modules
 
 		private static void ToggleEngine(RPPlayer player)
 		{
-			if (!player.LoggedIn || !player.IsInVehicle || player.Seat != 1) return;
+			if (!player.LoggedIn || !player.IsInVehicle || player.Vehicle.Driver != player) return;
 
 			var veh = (RPVehicle)player.Vehicle;
 
