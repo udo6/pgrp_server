bb72244cdee6cd55c1bc68969045c7625291e39d
Panda der echte <49002481+Pandaaa-C@users.noreply.github.com>
2023-12-20 14:18:01 +0000

    add controller

---
+++ backend/Game/Controllers/Jobs/MoneyTruckJobController.cs
@@ -0,0 +1,42 @@
+﻿using AltV.Net.Elements.Entities;
+using AltV.Net.Enums;
+using AltV.Net;
+using Core.Entities;
+using Database.Models.GarbageJob;
+using Database.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Enums;
+using Database.Models.MoneyTruckJob;
+using Core.Extensions;
+
+namespace Game.Controllers.Jobs
+{
+    public class MoneyTruckJobController
+    {
+        public static void LoadMoneyTruckJobs(MoneyTruckJobModel model)
+        {
+            var position = PositionService.Get(model.StartLocationId);
+            if (position == null) return;
+
+            var startJobShape = (RPShape)Alt.CreateColShapeCylinder(position.Position.Down(), 4f, 4f);
+            startJobShape.Id = model.Id;
+            startJobShape.ShapeType = ColshapeType.MONEY_TRUCK_JOB_START;
+            startJobShape.Size = 4f;
+
+            var ped = Alt.CreatePed(PedModel.Casey, position.Position, position.Rotation);
+            ped.Frozen = true;
+            ped.Health = 8000;
+            ped.Armour = 8000;
+
+            var blip = Alt.CreateBlip(true, BlipType.Destination, position.Position, Array.Empty<IPlayer>());
+            blip.Name = "Geldtransporter";
+            blip.Sprite = (ushort)67;
+            blip.Color = 46;
+            blip.ShortRange = true;
+        }
+    }
+}
---
+++ backend/Game/Controllers/Jobs/MoneyTruckJobRouteController.cs
@@ -0,0 +1,31 @@
+﻿using AltV.Net.Elements.Entities;
+using AltV.Net.Enums;
+using AltV.Net;
+using Core.Entities;
+using Database.Models.GarbageJob;
+using Database.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Enums;
+using Database.Models.MoneyTruckJob;
+using Core.Extensions;
+
+namespace Game.Controllers.Jobs
+{
+    public class MoneyTruckJobRouteController
+    {
+        public static void LoadMoneyTruckJobRoutes(MoneyTruckJobRouteModel model)
+        {
+            var position = PositionService.Get(model.PositionId);
+            if (position == null) return;
+
+            var pickupPoint = (RPShape)Alt.CreateColShapeCylinder(position.Position.Down(), 2f, 2f);
+            pickupPoint.Id = model.Id;
+            pickupPoint.ShapeType = ColshapeType.MONEY_TRUCK_JOB_PICKUP;
+            pickupPoint.Size = 2f;
+        }
+    }
+}
