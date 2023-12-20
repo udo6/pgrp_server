e50e0b5d2d0668fb0a9ad11390c96f68527f8d8b
mas on <dripudo123@gmail.com>
2023-12-20 23:20:13 +0100

    add ooc chat word blacklist

--- backend/Game/Commands/Player/PlayerCommands.cs
+++ backend/Game/Commands/Player/PlayerCommands.cs
@@ -5,9 +5,22 @@ namespace Game.Commands.Player
 {
 	public static class PlayerCommands
 	{
+		private static List<string> BlacklistedWords = new()
+		{
+			"nigga",
+			"nigger",
+			"hurensohn",
+		};
+
 		[Command("ooc", true)]
 		public static void OutOfCharacter(RPPlayer player, string message)
 		{
+			if(BlacklistedWords.Any(x => x == message.ToLower()))
+			{
+				player.Notify("Information", "Du hast versucht ein verbotenes Wort zu schreiben", Core.Enums.NotificationType.ERROR);
+				return;
+			}
+
 			foreach(var target in RPPlayer.All.ToList())
 			{
 				if (target.Position.Distance(player.Position) > 20f) continue;
