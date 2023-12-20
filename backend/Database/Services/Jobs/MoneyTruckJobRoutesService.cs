e0cdd40ceb420bb78484da7ff58d79b99e182d9b
Panda der echte <49002481+Pandaaa-C@users.noreply.github.com>
2023-12-20 14:22:23 +0000

    rename route service

--- backend/Database/Services/Jobs/MoneyTruckJobRoutesService.cs
+++ backend/Database/Services/Jobs/MoneyTruckJobRoutesService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Database.Services.Jobs
 {
-    public class GarbageJobRoutesService
+    public class MoneyTruckJobRoutesService
     {
         public static List<MoneyTruckJobRouteModel> GetAll()
         {
