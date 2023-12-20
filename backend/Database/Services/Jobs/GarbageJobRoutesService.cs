f59b27b639cc7236b815c35b37441caaea8e9ed3
Panda der echte <49002481+Pandaaa-C@users.noreply.github.com>
2023-12-20 14:12:38 +0000

    add services

---
+++ backend/Database/Services/Jobs/GarbageJobRoutesService.cs
@@ -0,0 +1,39 @@
+﻿using Database.Models.GarbageJob;
+using Database.Models.MoneyTruckJob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Services.Jobs
+{
+    public class GarbageJobRoutesService
+    {
+        public static List<MoneyTruckJobRouteModel> GetAll()
+        {
+            using var ctx = new Context();
+            return ctx.MoneyTruckJobRoutes.ToList();
+        }
+
+        public static void Add(MoneyTruckJobRouteModel model)
+        {
+            using var ctx = new Context();
+            ctx.MoneyTruckJobRoutes.Add(model);
+            ctx.SaveChanges();
+        }
+
+        public static void Remove(MoneyTruckJobRouteModel model)
+        {
+            using var ctx = new Context();
+            ctx.MoneyTruckJobRoutes.Remove(model);
+            ctx.SaveChanges();
+        }
+
+        public static MoneyTruckJobRouteModel? Get(int id)
+        {
+            using var ctx = new Context();
+            return ctx.MoneyTruckJobRoutes.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
