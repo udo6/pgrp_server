f597ddf16b9fa2af710ffbdc0e65b40077daf075
mas on <dripudo123@gmail.com>
2024-01-07 22:48:16 +0100

    add inventory command

--- backend/Game/Commands/Admin/InventoryCommands.cs
+++ backend/Game/Commands/Admin/InventoryCommands.cs
@@ -1,7 +1,12 @@
 ﻿using Core.Attribute;
 using Core.Entities;
+using Core.Extensions;
+using Database.Models.Account;
+using Database.Models.Inventory;
 using Database.Services;
 using Game.Controllers;
+using Game.Modules;
+using Newtonsoft.Json;
 
 namespace Game.Commands.Admin
 {
@@ -32,6 +37,33 @@ namespace Game.Commands.Admin
 			InventoryService.ClearInventoryItems(target.InventoryId);
 			player.Notify("Administration", $"Du hast das Inventar von {target.Name} gecleared!", Core.Enums.NotificationType.SUCCESS);
 			target.Notify("Administration", $"Dein Inventar wurde von {player.Name} gecleared!", Core.Enums.NotificationType.WARN);
+		}
+
+		[Command("openinventory")]
+		public static void OpenInventory(RPPlayer player, int inventoryId)
+		{
+			if (player.AdminRank < Core.Enums.AdminRank.ADMINISTRATOR) return;
+
+			var itemBases = InventoryService.GetItems();
+			var inventory = InventoryService.Get(player.InventoryId);
+			var container = InventoryService.Get(inventoryId);
+			if (inventory == null || container == null) return;
+
+			var inventoryItems = InventoryService.GetInventoryItems2(inventory.Id, container.Id);
+			var invItems = InventoryController.GetInventoryItems(inventoryItems.Items1, itemBases);
+			var ctnItems = InventoryController.GetInventoryItems(inventoryItems.Items2, itemBases);
+
+			player.ShowComponent("Inventory", true, JsonConvert.SerializeObject(new
+			{
+				Inventory = inventory,
+				InventoryItems = invItems,
+				ContainerLabel = InventoryModule.GetContainerLabel(container.Type),
+				Container = container,
+				ContainerItems = ctnItems,
+				Loadout = Array.Empty<object>(),
+				GiveItemTarget = -1,
+				SearchTargetId = -1
+			}));
 		}
 	}
 }
